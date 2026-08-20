bbf20e8dee10d095553a426015d37f2a7763110f
amanbaevrahat917-coder <amanbaevrahat917@gmail.com>
2026-08-20 16:10:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,8 @@ if user_input := st.chat_input("Ваше сообщение:"):
         st.write(user_input)
 
     response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        model="llama3-8b-8192",
+,
         messages=st.session_state.messages
     )
     bot_reply = response.choices[0].message.content
